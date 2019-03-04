7160c57f659d7c93b84e9b1c84a941aa98d7dcb5
jonathan <jmitchell1991@gmail.com>
2019-03-03 23:13:11 -0800

    [feature] - fixes preprocessor

--- data_generator.py
+++ data_generator.py
@@ -23,36 +23,37 @@ def generate_training_data(data, config, batch_size=4):
             row = data.iloc[idx]
 #             data.reset_index() 
             x = preprocess_image(row['image_id'], to_height=config.IMG_SIZE, to_width=config.IMG_SIZE)
-            # x = normalize(x, kind='mean_std')
             x = x / 255.
             image_batch[i] = np.reshape(x, (config.IMG_SIZE*config.IMG_SIZE*config.IMG_CHANNEL))
         yield image_batch, image_batch
 # Note: you can make this convolutional without flattening
 
-# def generate_validation_data(data, config, batch_size=4):
-#     if config is None:
-#         raise ValueError('inside gen train data config is None')
-    
-#     image_batch = np.zeros((batch_size,config.IMG_SIZE, config.IMG_SIZE, config.IMG_CHANNEL))
-#     while True:
-#         for i in range(batch_size):
-#             idx = np.random.randint(len(data))
-#             row = data.iloc[idx]
-# #             data.reset_index() 
-#             x = preprocess_image_val(row['image_id'], to_height=config.IMG_SIZE, to_width=config.IMG_SIZE)
-#             image_batch[i] = normalize(x, kind='max_min')
-            
-            
-#         yield image_batch, image_batch
-
-def generate_validation_data(data, config):
+def generate_validation_data(data, config, batch_size=4):
     if config is None:
         raise ValueError('inside gen train data config is None')
+    
+    image_batch = np.zeros((batch_size,config.IMG_SIZE*config.IMG_SIZE* config.IMG_CHANNEL))
     while True:
+        for i in range(batch_size):
             idx = np.random.randint(len(data))
             row = data.iloc[idx]
 #             data.reset_index() 
             x = preprocess_image_val(row['image_id'], to_height=config.IMG_SIZE, to_width=config.IMG_SIZE)
-            x = normalize(x, kind='max_min')
+            x = x / 255.
+            image_batch[i] = np.reshape(x, (config.IMG_SIZE*config.IMG_SIZE*config.IMG_CHANNEL))
+
             
-            yield x, x
+            
+        yield image_batch, image_batch
+
+# def generate_validation_data(data, config):
+#     if config is None:
+#         raise ValueError('inside gen train data config is None')
+#     while True:
+#             idx = np.random.randint(len(data))
+#             row = data.iloc[idx]
+# #             data.reset_index() 
+#             x = preprocess_image_val(row['image_id'], to_height=config.IMG_SIZE, to_width=config.IMG_SIZE)
+#             x = normalize(x, kind='max_min')
+            
+#             yield x, x
