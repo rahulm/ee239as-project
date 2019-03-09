3e70c892fff84c2cb9ac3021285d349b712a027a
Calvin Pham <calvinpham@cs.ucla.edu>
2019-03-08 16:26:43 -0800

    Fixed loss function

--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@ import numpy as np
 from appearance_vae import appearance_VAE
 import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
 
 
 all_face_images_warped = np.load('all-warped-images.npy')
@@ -14,7 +15,7 @@ app_vae = appearance_VAE(latent_dim_size=50)
 app_vae.load_state_dict(torch.load("./experiments/2019_03_08-01_37_45-experiment/models/Appearance-VAE-weights-epoch_4.pth", map_location=lambda storage, loc: storage)())
 
 app_vae.eval()
-selected_img = app_vae(ImgToTensor()(np.copy(face_images_test_warped[12])).unsqueeze(0))
+selected_img = app_vae(ImgToTensor()(np.copy(face_images_train_warped[12])).unsqueeze(0))
 sample_app_recon = selected_img[0].squeeze().data.cpu().numpy().transpose((1, 2, 0))
 
 plt.imshow(sample_app_recon)
