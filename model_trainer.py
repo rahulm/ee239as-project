6d4d1d636063497547eb953dffc06ba88d8d0376
Rahul Malavalli <rahul.malavalli@gmail.com>
2019-03-13 19:27:29 -0700

    removed normalization of vae_loss by batch size

--- model_trainer.py
+++ model_trainer.py
@@ -168,8 +168,8 @@ class vae_trainer(object):
         KLD_element = mu.pow(2).add_(var.exp()).mul_(-1).add_(1).add_(var)
         KLD = torch.sum(KLD_element).mul_(-0.5)
 
-        return (recon_loss + KLD) / float(len(x))
-        # return recon_loss + KLD
+        # return (recon_loss + KLD) / float(len(x))
+        return recon_loss + KLD
 
     def train_model(self, epochs, trainloader, valloader, testloader, checkpoint_interval, test_samples, test_tensors, all_samples, recon_gen_interval, num_gen, **kwargs):
         # self.model.train()
